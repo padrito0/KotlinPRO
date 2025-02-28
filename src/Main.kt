c607584bce919c4a1000f7f40b77bc8cab858155
padrito0 <ap0978202@otc.edu>
2025-02-27 19:45:31 -0600

    Initial commit of Kotlin BookFinder project

--- src/Main.kt
+++ src/Main.kt
@@ -21,14 +21,21 @@ fun findBookByTitle(title: String): Book? {
 }
 
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("\n Books we have available:")
+    for (book in bookList) {
+        book.printDetails()
+    }
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    println("\n What book are you looking for?")
+    val userInput = readlnOrNull()?.trim()
+
+    println("You entered -> '$userInput'")
+
+    if (userInput.isNullOrBlank()) {
+        println("We don't have that book. Try another one!")
+    } else {
+        val foundBook = findBookByTitle(userInput)
+        foundBook?.printDetails() ?: println("Book not found.")
     }
 }
+
